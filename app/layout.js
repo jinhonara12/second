82df956579614d34e654082da12fd706c06400bd
jinho <jinhonara12@naver.com>
2024-05-21 02:35:03 +0900

    runtime +

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ import styles from './layout.module.scss';
 import './reset.css';
 import './global.scss';
 
-export const maxDuration = 5; // This function can run for a maximum of 5 seconds
+export const maxDuration = 60; // This function can run for a maximum of 5 seconds
 export const dynamic = 'force-dynamic';
 
 export function GET(request) {
