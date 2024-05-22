1a1b096b942cfb311663f6a6d2b776f14c5bd8d1
jinho <jinhonara12@naver.com>
2024-05-23 02:02:32 +0900

    error handdle +

--- app/not-found.js
+++ app/not-found.js
@@ -2,7 +2,7 @@ import styles from './layout.module.scss'
 
 export default function NotFound() {
     return (
-        <div className={styles.not_found}>
+        <div className={styles.error_page}>
             <h2>존재하지 않는 페이지입니다.</h2>
         </div>
     )
