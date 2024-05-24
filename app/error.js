50c9fd94971bb627d338726ff465fec296d62483
jinho <jinhonara12@naver.com>
2024-05-24 17:47:37 +0900

    error ux change

--- app/error.js
+++ app/error.js
@@ -1,7 +1,6 @@
 'use client' // Error components must be Client Components
 
 import { useEffect } from 'react'
-
 import styles from './layout.module.scss'
 
 export default function Error({ error, reset }) {
@@ -11,8 +10,24 @@ export default function Error({ error, reset }) {
 
     return (
         <div className={styles.error_page}>
-            <p>서버 응답이 지연되고 있습니다.</p>
-            <p>이 문제는 10초 이상의 지연이 생겼을 때 발생하는 오류로 새로고침하여 다시 접속해주시기 바랍니다.</p>
+            <div className={styles.title}>
+                <h2>500 : Got a problem?</h2>
+                <p>문제가 생겼나요?</p>
+            </div>
+            <div className={styles.content}>
+                <div className={styles.text_box}>
+                    <p>문제가 지속적으로 발생한다면 데일리스윙에게 알려주세요.</p>
+                    <div className={styles.small}>
+                        <p>데일리스윙은 린디하퍼 커뮤니티를 지향하고 있어요.</p>
+                        <p>가끔 길을 헤매더라도 함께 할 수 있는 공간을 만들고 있습니다.</p>
+                    </div>
+
+                </div>
+                <div className={styles.link_box}>
+                    <Link href="/">처음으로 돌아가기</Link>
+                    <a href={process.env.GOOGLE_DATA_FORM} target="_blank">오류 제보하기</a>
+                </div>
+            </div>
         </div>
     )
 }
--- app/not-found.js
+++ app/not-found.js
@@ -1,9 +1,27 @@
+import Link from 'next/link'
 import styles from './layout.module.scss'
 
 export default function NotFound() {
     return (
         <div className={styles.error_page}>
-            <h2>존재하지 않는 페이지입니다.</h2>
+            <div className={styles.title}>
+                <h2>404 : Are you lost?</h2>
+                <p>길을 잃으셨나요?</p>
+            </div>
+            <div className={styles.content}>
+                <div className={styles.text_box}>
+                    <p>찾으시는 페이지가 없다면 데일리스윙에게 알려주세요.</p>
+                    <div className={styles.small}>
+                        <p>데일리스윙은 린디하퍼의 커뮤니티 지향하고 있어요.</p>
+                        <p>가끔 길을 헤매더라도 함께 할 수 있는 공간을 만들고 있습니다.</p>
+                    </div>
+
+                </div>
+                <div className={styles.link_box}>
+                    <Link href="/">처음으로 돌아가기</Link>
+                    <a href={process.env.GOOGLE_DATA_FORM} target="_blank">원하는 내용 알려주기</a>
+                </div>
+            </div>
         </div>
     )
 }
